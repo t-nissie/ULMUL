a8dea9bc9a46809cd75e9c479df3b97f2da98e39
t-nissie <t-nissie@yahoo.co.jp>
2010-10-07 12:32:18 +0000

    ulmul_test.rb: assert(Ulmul.method_defined?(:parse))
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ulmul/ulmul/trunk@65 591d1d34-8138-47ad-a474-ac4e47b36248

--- test/unit/ulmul_test.rb
+++ test/unit/ulmul_test.rb
@@ -1,5 +1,5 @@
 # ulmul_test.rb -*-ruby-*-
-# Time-stamp: <2010-10-06 20:45:46 takeshi>
+# Time-stamp: <2010-10-07 21:29:34 takeshi>
 # Author: Takeshi NISHIMATSU
 ##
 require 'rubygems'
@@ -7,19 +7,22 @@ require 'test/unit'
 require 'ulmul'
 
 class TestULMUL < Test::Unit::TestCase
+
   def test_exist_Ulmul
     assert_instance_of(Class, Ulmul)
   end
+
+  def test_method_parse
+    assert(Ulmul.method_defined?(:parse))
+  end
+
+  def test_method_html
+    assert(Ulmul.method_defined?(:html))
+  end
+
   def test_singleton_new
     u = Ulmul.new(2..3,'ulmul2html5')
     assert_instance_of(Ulmul, u)
   end
-  def test_method_parse
-    u = Ulmul.new(2..3,'ulmul2xhtml')
-    assert_respond_to(u,:parse)
-  end
-  def test_method_html
-    u = Ulmul.new(2..3,'ulmul2xhtml')
-    assert_respond_to(u,:html)
-  end
+
 end
